4087d9712a0d871ed0de247918e12b5141591f33
zenyuong <zenyu.ong.2020@scis.smu.edu.sg>
2023-03-21 01:51:17 +0800

    cors workflow

--- src/main/java/com/group1/oopproject/workflow/controller/WorkflowController.java
+++ src/main/java/com/group1/oopproject/workflow/controller/WorkflowController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.group1.oopproject.workflow.service.WorkflowService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/workflow")
 public class WorkflowController {
